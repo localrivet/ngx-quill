211137087f7289c5c0b531e900ee8a9ebeac1bb8
Alma Tuck <alma.tuck@gmail.com>
2020-09-28 10:45:04 -0600

    working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { QuillModule } from 'ngx-quill';
-import {QuillInitializeService} from "./services/quillInitialize.service";
+import { QuillInitializeService } from "./services/quillInitialize.service";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, QuillModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ],
+  imports: [BrowserModule, FormsModule, QuillModule.forRoot()],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
   providers: [QuillInitializeService]
 })
 export class AppModule { }
